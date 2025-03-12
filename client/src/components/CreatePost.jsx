92b72ae2c7dee4cd7753610081a7ce3a34d57b7b
anshu <anshusnp2004@gmail.com>
2025-03-13 01:20:02 +0530

    created post api route

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -32,10 +32,9 @@ export default function CreatePost() {
         />
         <Button
           type='button'
-          gradientDuoTone='purpleToBlue'
-          size='sm'
-          outline
+          className='mx-3 rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-extrabold'
         >
+            Upload image
         </Button>
       </div>
       <ReactQuill
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { configDotenv } from 'dotenv'
 import router from './routes/user.route.js'
 import auth from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
+import post from './routes/post.route.js'
 const app=express()
 configDotenv()
 app.use(express.json())
@@ -26,6 +27,7 @@ app.get('/',(req,res)=>{
 app.use("/api/user",router)
 app.use("/api/auth",auth)
 app.use("/api/auth",auth)
+app.use("/api/post",post)
 app.use((error,req,res,next)=>{
     const errcode=error.statusCode || 500
     const errmsg=error.message || "internal Time server error"
