b0f435e3c77addd0105df6f79e6288e27bb25f8d
anshu <anshusnp2004@gmail.com>
2025-03-14 13:42:45 +0530

    show users to admin

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPost from '../components/DashPost';
-
+import DashUsers from '../components/DashUsers';
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
@@ -24,6 +24,8 @@ export default function Dashboard() {
       {tab === 'profile' && <DashProfile />}
       {/* posts... */}
       {tab === 'posts' && <DashPost />}
+        {/* users... */}
+        {tab === 'users' && <DashUsers />}
     </div>
   );
 }
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -2,46 +2,46 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { FaCheck, FaTimes } from 'react-icons/fa';
 import { Modal ,Button} from 'flowbite-react';
 
-export default function DashPost() {
+export default function DashUsers() {
   const { currentUser } = useSelector((state) => state.user);
-  const [userPosts, setUserPosts] = useState([]);
+  const [users, setUsers] = useState([]);
   const [showMore, setShowMore] = useState(true)
   const [showModal, setShowModal] = useState(false);
-  const [postIdToDelete, setPostIdToDelete] = useState('');
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchUsers = async () => {
       try {
-        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const res = await fetch('/api/user/getusers');
         const data = await res.json();
+        console.log(data)
         if (res.ok) {
-          setUserPosts(data.posts);
-          console.log(data)
-          if (data.posts.length < 9) {
+          setUsers(data.users);
+          if (data.users.length < 9) {
             setShowMore(false);
           }
         }
       } catch (error) {
-        console.log(error.message);
+        console.log("error.message");
       }
     };
 
     if (currentUser.isAdmin) {
-      fetchPosts();
+      fetchUsers();
     }
   }, [currentUser._id]);
   const handleShowMore = async () => {
-    const startIndex = userPosts.length;
+    const startIndex = users.length;
     try {
       const res = await fetch(
-        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+        `/api/auth/getusers?startIndex=${startIndex}`
       );
       const data = await res.json();
       if (res.ok) {
-        setUserPosts((prev) => [...prev, ...data.posts]);
-        if (data.posts.length < 9) {
+        setUsers((prev) => [...prev, ...data.users]);
+        if (data.users.length < 9) {
           setShowMore(false);
         }
       }
@@ -49,89 +49,56 @@ export default function DashPost() {
       console.log(error.message);
     }
   };
-  const handleDeletePost = async () => {
-    setShowModal(false);
-    try {
-      const res = await fetch(
-        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
-        {
-          method: 'DELETE',
-        }
-      );
-      const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
-      } else {
-        setUserPosts((prev) =>
-          prev.filter((post) => post._id !== postIdToDelete)
-        );
-      }
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
 
   return (
     <div className='table-auto w-full overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
-      {currentUser.isAdmin && userPosts.length > 0 ? (
+      {currentUser.isAdmin && users.length > 0 ? (
         <>
         <table className='min-w-full divide-y divide-gray-200 shadow-md rounded-lg overflow-hidden'>
           <thead className='bg-gray-50 dark:bg-gray-700'>
             <tr>
               <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider'>
-                Date updated
+                Date created
               </th>
               <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider'>
-                Post image
+                user image
               </th>
               <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider'>
-                Post title
+                username
               </th>
               <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider'>
-                Category
+                admin
               </th>
               <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider'>
                 Delete
               </th>
-              <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider'>
-                Edit
-              </th>
             </tr>
           </thead>
           <tbody className='bg-white dark:bg-gray-800 divide-y divide-gray-200'>
-            {userPosts.map((post) => (
-              <tr key={post._id} className='hover:bg-gray-50 dark:hover:bg-gray-700'>
+            {users.map((user) => (
+              <tr key={user._id} className='hover:bg-gray-50 dark:hover:bg-gray-700'>
                 <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100'>
-                  {new Date(post.updatedAt).toLocaleDateString()}
+                  {new Date(user.createdAt).toLocaleDateString()}
                 </td>
                 <td className='px-6 py-4 whitespace-nowrap'>
-                  <Link to={`/post/${post.slug}`}>
-                    <img
-                      src={post.image}      
-                      alt={post.title}
-                      className='w-20 h-10 object-cover bg-gray-500 rounded'
-                    />
-                  </Link>
+                    <img className='w-12 h-12 object-cover bg-gray-500 rounded-full' src={user.profilePicture} alt='image'></img>
+  
                 </td>
                 <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100'>
-                  <Link
-                    className='font-medium text-blue-600 dark:text-blue-400 hover:underline'
-                    to={`/post/${post.slug}`}
-                  >
-                    {post.title}
-                  </Link>
+                    {user.username}
+    
                 </td>
                 <td className='px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100'>
-                  {post.category}
+                {user.isAdmin ? (
+                      <FaCheck className='text-green-500' />
+                    ) : (
+                      <FaTimes className='text-red-500' />
+                    )}
                 </td>
                 <td className='px-6 py-4 whitespace-nowrap text-sm text-red-600 dark:text-red-400 hover:underline cursor-pointer'>
-                  <span onClick={() => {setShowModal(true);setPostIdToDelete(post._id)}} >
+                  <span onClick={() => {setShowModal(true)}} >
                     Delete
                   </span>
-                </td>
-                <td className='px-6 py-4 whitespace-nowrap text-sm text-teal-600 dark:text-teal-400 hover:underline'>
-                  <Link to={`/update-post/${post._id}`}>Edit</Link>
                 </td>
               </tr>
             ))}
@@ -148,7 +115,7 @@ export default function DashPost() {
               </>
         
       ) : (
-        <p className='text-gray-700 dark:text-gray-300'>You have no posts yet!</p>
+        <p className='text-gray-700 dark:text-gray-300'>You have no users</p>
       )}
       <Modal
         show={showModal}
@@ -163,7 +130,7 @@ export default function DashPost() {
               Are you sure you want to delete this post?
             </h3>
             <div className='flex justify-center gap-4'>
-              <Button className="bg-red-600 text-white " onClick={handleDeletePost}>
+              <Button className="bg-red-600 text-white ">
                 Yes, I'm sure
               </Button>
               <Button color='gray' onClick={() => setShowModal(false)}>
