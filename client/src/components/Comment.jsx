0fd4976eae3f1b0446f0412edb4ff0327cf68c92
anshu <anshusnp2004@gmail.com>
2025-03-14 21:11:25 +0530

    added like functionality to comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -2,9 +2,10 @@ import { useState,useEffect } from "react";
 import { useSelector } from "react-redux";
 import { FaThumbsUp } from 'react-icons/fa';
 import moment from 'moment';
-export default function Comment({ comment}) {
+export default function Comment({ comment, onLike}) {
     const [user, setUser] = useState({});
     const { currentUser } = useSelector((state) => state.user);
+    
     useEffect(() => {
       const getUser = async () => {
         try {
@@ -41,6 +42,7 @@ export default function Comment({ comment}) {
             <div className='flex items-center pt-2 text-xs  dark:border-gray-700 max-w-fit gap-2'>
               <button
                 type='button'
+                onClick={() => onLike(comment._id)}
                 className={`text-gray-400 hover:text-blue-500 ${
                   currentUser &&
                   comment.likes.includes(currentUser._id) &&
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -10,6 +10,33 @@ export default function CommentSection({postId}) {
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const handleLike = async (commentId) => {
+      try {
+        if (!currentUser) {
+          navigate('/sign-in');
+          return;
+        }
+        const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+          method: 'PUT',
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setComments(
+            comments.map((comment) =>
+              comment._id === commentId
+                ? {
+                    ...comment,
+                    likes: data.likes,
+                    numberOfLikes: data.likes.length,
+                  }
+                : comment
+            )
+          );
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
     useEffect(() => {
         const getComments = async () => {
           try {
@@ -118,6 +145,7 @@ export default function CommentSection({postId}) {
             <Comment
               key={comment._id}
               comment={comment}
+              onLike={handleLike} 
             />
           ))}
         </>
