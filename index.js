71477e36f72913a02ee32388a2a472bc88f3b6d2
anshu <anshusnp2004@gmail.com>
2025-03-16 01:39:15 +0530

    test

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use("/api/post",post)
 app.use("/api/comment",commentRoutes)
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
